28c3667045f027ce0eb111c7d6c5a0b070955b44
dongzeyuan <dongfujing88@gmail.com>
2018-04-07 17:05:42 +0800

    增加config配置文件

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
+# encoding:utf-8
+
 from flask import Flask
+import config
+
 app = Flask(__name__)
+app.config.from_object(config)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>config 配置!</h1>'
 
 
 @app.route('/user/<name>')
@@ -13,4 +18,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
