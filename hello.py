ea158ae81603c097375a35f0b1b7fc9a2f718131
dongzeyuan <dongfujing88@gmail.com>
2018-04-08 14:18:05 +0800

    template

--- hello.py
+++ hello.py
@@ -1,21 +1,25 @@
 # encoding:utf-8
 
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.script import Manager
 import config
+
+
 
 app = Flask(__name__)
 app.config.from_object(config)
+manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>config 配置!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
