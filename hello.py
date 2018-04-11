6b11af23ccf577123f606e3674e129aae9e426c6
dongzeyuan <dongfujing88@gmail.com>
2018-04-11 16:05:53 +0800

    aa

--- hello.py
+++ hello.py
@@ -1,23 +1,27 @@
 # encoding:utf-8
 
 from flask import Flask, render_template
+from flask.ext.wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name', validators=[Required()])
+    submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template("index.html")
-
-
-@app.route('/projects/')
-def projects():
-    return "projects page"
-
-
-@app.route('/about')
-def about():
-    return "about page"
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form,name=name)
 
 
 if __name__ == '__main__':
