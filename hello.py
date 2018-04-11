d8b831a85e0d836d4999d058a9792f7aa8cadb6e
dongzeyuan <dongfujing88@gmail.com>
2018-04-11 11:45:52 +0800

    创建note

--- hello.py
+++ hello.py
@@ -1,24 +1,23 @@
 # encoding:utf-8
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "index"
+    return render_template("index.html")
+
 
 @app.route('/projects/')
 def projects():
     return "projects page"
 
+
 @app.route('/about')
 def about():
     return "about page"
-
-
-
 
 
 if __name__ == '__main__':
---
+++ note.md
@@ -0,0 +1,11 @@
+# 安装
+## 安装flask
+1. 创建虚拟环境：
+* 启动cmd，切记之后所有的操作都是在cmd中执行，不是在powershell中执行
+* 切换到想要创建虚拟环境文件夹的目录，例如C:\Python36
+* 执行如下命令
+    cmd
+    mkdir python-env
+    cd python-env
+    python -m venv flask-env
+2. 在vs code中使用虚拟环境
