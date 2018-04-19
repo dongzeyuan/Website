1037f81c101df3474e1a290bcc8a01ffecc553a5
dongzeyuan <dongfujing88@gmail.com>
2018-04-19 22:12:37 +0800

    D2 终于能跑起来了

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,26 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
 from config import config
+
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+# 所谓的工厂函数，接受一个参数，是程序使用的配置名
+def create_app(config_name):
+    app = Flask(__name__)
+    # config.from_object（）函数接收一个外部的config文件名称
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
