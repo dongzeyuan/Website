5be583855990c040e722171a3ad8705f585d4e07
dongzeyuan <dongfujing88@gmail.com>
2018-04-24 14:56:44 +0800

    D7 添加注释

--- config.py
+++ config.py
@@ -7,9 +7,12 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.163.com'
+    # 这里端口和书上不一样，国内163邮箱是465
     MAIL_PORT = 465
+    # 使用SSL协议而不是TLS协议，和书上也不一样
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # 环境变量中设置的密码是第三方客户端的授权码
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
