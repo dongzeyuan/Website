5ac15d33fe5342117d585c9cc235fcba03d85266
dongzeyuan <dongfujing88@gmail.com>
2018-05-13 13:15:58 +0800

    水一发

--- app/models.py
+++ app/models.py
@@ -203,6 +203,15 @@ class AnonymousUser(AnonymousUserMixin):
         return False
 
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 login_manager.anonymous_user = AnonymousUser
 
 
