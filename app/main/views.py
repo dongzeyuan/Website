5a60f43a6734e7da0b5e915928884b673113a710
dongzeyuan <dongfujing88@gmail.com>
2018-05-19 21:36:31 +0800

    D26 对应12e。chapter 12 ending

--- app/main/views.py
+++ app/main/views.py
@@ -154,7 +154,7 @@ def unfollow(username):
 
 
 @main.route('/folllowers/<username>')
-def followers(usernanme):
+def followers(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('Invalid user.')
@@ -186,6 +186,7 @@ def followed_by(username):
                            endpoint='.followed_by', pagination=pagination,
                            follows=follows)
 
+
 @main.route('/all')
 @login_required
 def show_all():
@@ -193,9 +194,10 @@ def show_all():
     resp.set_cookie('show_followed', '', max_age=30*24*60*60)
     return resp
 
+
 @main.route('/followed')
 @login_required
 def show_followed():
     resp = make_response(redirect(url_for('.index')))
-    resp.set_cookie('show_followed','1',max_age=30*24*60*60)
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
     return resp
--- app/models.py
+++ app/models.py
@@ -124,6 +124,7 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+        self.follow(self)
 
     @property
     def password(self):
@@ -229,6 +230,14 @@ class User(UserMixin, db.Model):
     def is_followed_by(self, user):
         return self.followers.filter_by(
             follower_id=user.id).first() is not None
+
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
     @property
     def followed_posts(self):
