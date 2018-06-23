786acd52025c54c56f85403e5b7fd2d76f42ffd1
dongzeyuan <dongfujing88@gmail.com>
2018-06-23 23:32:15 +0800

    better

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,10 +3,19 @@ from flask import render_template
 from . import main
 
 # 使用蓝图定义错误路由
+
+
 @main.app_errorhandler(404)
 def page_not_found(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        reponse = jsonify({'error': 'not found'})
+        reponse.status_code = 404
+        return response
     return render_template('404.html'), 404
 # 使用蓝图定义错误路由
+
+
 @main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
