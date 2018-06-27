a744032d421a411f3f88496bd4d68814e24e771b
dongzeyuan <dongfujing88@gmail.com>
2018-06-27 10:35:10 +0800

    這一章真看不懂

--- app/models.py
+++ app/models.py
@@ -232,6 +232,20 @@ class User(UserMixin, db.Model):
         return self.followers.filter_by(
             follower_id=user.id).first() is not None
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],
+                       expires_in=expiration)
+        return s.dumps({'id':self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
     @staticmethod
     def add_self_follows():
         for user in User.query.all():
