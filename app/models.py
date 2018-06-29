165c4a05ac519aaf05b40acafcb728ac61fd1f4d
dongzeyuan <dongfujing88@gmail.com>
2018-06-29 11:01:25 +0800

    这本书完结了，我需要开启一个新项目

--- app/models.py
+++ app/models.py
@@ -237,6 +237,19 @@ class User(UserMixin, db.Model):
                        expires_in=expiration)
         return s.dumps({'id': self.id})
 
+    def to_json(self):
+        json_user = {
+            'url': url_for('api.get_post', id=self.id, _external=True),
+            'username': self.username,
+            'member_since': self.member_since,
+            'last_seen': self.last_seen,
+            'posts': url_for('api.get_posts', id=self.id, _external=True),
+            'followed_posts': url_for('api.get_user_followed_posts',
+                                      id=self.id, _external=True),
+            'post_count': self.posts.count()
+        }
+        return json_user
+
     @staticmethod
     def verify_auth_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
